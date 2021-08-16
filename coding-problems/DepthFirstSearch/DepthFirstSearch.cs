9c5cd285813a268db75404bcad31ab8baa607c7d
Steven Garcia <stevgarcia2015@gmail.com>
2021-08-16 09:35:41 -0700

    Add solution for depth first search
    Add missing closing bracket

--- coding-problems/DepthFirstSearch/DepthFirstSearch.cs
+++ coding-problems/DepthFirstSearch/DepthFirstSearch.cs
@@ -1,7 +1,50 @@
+using System;
+using System.Collections.Generic;
+
 namespace coding_problems.DepthFirstSearch
 {
     public class DepthFirstSearch
     {
+        // Do not edit the class below except
+        // for the DepthFirstSearch method.
+        // Feel free to add new properties
+        // and methods to the class.
+        public class Node
+        {
+            public string name;
+            public List<Node> children = new List<Node>();
 
+            public Node(string name)
+            {
+                this.name = name;
+            }
+
+            public List<string> DepthFirstSearch(List<string> array)
+            {
+                if (children.Count == 0) return array;
+
+                Stack<Node> stack = new Stack<Node>();
+                stack.Push(children[0]);
+
+                while (stack.Count > 0)
+                {
+                    Node top = stack.Pop();
+                    array.Add(top.name);
+                    Console.WriteLine(top.name);
+                    foreach (Node childNode in top.children)
+                        stack.Push(childNode);
+                }
+
+                return array;
+            }
+
+            public Node AddChild(string name)
+            {
+                Node child = new Node(name);
+                children.Add(child);
+                return this;
+            }
+
+        }
     }
 }
